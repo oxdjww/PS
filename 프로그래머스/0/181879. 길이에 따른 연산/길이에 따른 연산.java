138cbadb062742fa563e25614e438780b2084095
JungTae Kwon <oxdjww@gmail.com>
2024-12-17 18:23:22 +0900

    [level 0] Title: 길이에 따른 연산, Time: 1.03 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,9 @@
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        return num_list.length >= 11
+            ? IntStream.of(num_list).sum()
+            : IntStream.of(num_list).reduce(1, (acc, i) -> acc * i);
+    }
+}
