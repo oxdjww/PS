09b65e0a094ceabda8677aa18e3fadc2a7bbb067
JungTae Kwon <oxdjww@gmail.com>
2024-12-18 02:20:29 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ; i < str1.length()*2; i++)
+        {
+            sb.append((i % 2 == 0) ? str1.charAt(i / 2) : str2.charAt(i / 2));
+        }
+        return sb.toString();
+    }
+}
