7573e0522d48383b05e7d231565e1e5d4b07d581
JungTae Kwon <oxdjww@gmail.com>
2024-12-18 01:55:44 +0900

    [level 0] Title: 문자열 돌리기, Time: 152.21 ms, Memory: 66.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] arr = sc.next().toCharArray();
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
