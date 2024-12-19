0334c48805d697db09603345d680306c090762c9
JungTae Kwon <oxdjww@gmail.com>
2024-12-20 00:49:21 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.55 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n, String control) {
+        char[] arr = control.toCharArray();
+        for (int i = 0 ; i < arr.length ; i++)
+        {
+            if (arr[i] == 'w')
+            {
+                n++;
+            }
+            else if (arr[i] == 's')
+            {
+                n--;
+            }
+            else if (arr[i] == 'd')
+            {
+                n += 10;
+            }
+            else if (arr[i] == 'a')
+            {
+                n -= 10;
+            }
+        }
+        return n;
+    }
+}
