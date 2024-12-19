2fad13ed872ca229b4b41259f444f8da31df37db
JungTae Kwon <oxdjww@gmail.com>
2024-12-19 22:39:10 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 5.30 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -3,16 +3,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 class Solution {
     public int solution(int a, int d, boolean[] included) {
-        int answer = 0;
-        List<Integer> arr = IntStream.range(0, included.length)
-            .filter(i -> included[i])
-            .boxed()
-            .collect(Collectors.toList());
-        for(Integer idx : arr)
-        {
-            answer += (a + idx * d);
-        }
-        
-        return answer;
+        return IntStream.range(0, included.length)
+            .map(i -> included[i]? a + i * d:0).sum();
     }
 }
