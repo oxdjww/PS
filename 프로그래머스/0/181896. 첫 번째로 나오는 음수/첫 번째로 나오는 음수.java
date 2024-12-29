8ff093f3e2863b6627c4c40a3bd3fd60b0c15b88
JungTae Kwon <oxdjww@gmail.com>
2024-12-29 15:29:37 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 2.54 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -2,9 +2,9 @@ import java.util.*;
 import java.util.stream.*;
 class Solution {
     public int solution(int[] num_list) {
-        OptionalInt first = IntStream.range(0, num_list.length)
+        return IntStream.range(0, num_list.length)
             .filter(idx -> num_list[idx] < 0)
-            .findFirst();
-        return first.isPresent() ? first.getAsInt() : -1;
+            .findFirst()
+            .orElse(-1);
     }
 }
