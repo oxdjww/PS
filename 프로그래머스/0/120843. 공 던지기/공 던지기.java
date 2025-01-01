f2468224ed92f140e8cc02559c5dd8d308dcdd48
JungTae Kwon <oxdjww@gmail.com>
2025-01-01 22:30:58 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,13 +1,14 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int cnt = 0;
-        int idx = 0;
-        while(cnt != k)
-        {
-            idx += 2;
-            idx %= numbers.length;
-            cnt++;
-        }
-        return idx - 2 < 0 ? numbers[idx - 2 + numbers.length] : numbers[idx - 2];
+        // int cnt = 0;
+        // int idx = 0;
+        // while(cnt != k)
+        // {
+        //     idx += 2;
+        //     idx %= numbers.length;
+        //     cnt++;
+        // }
+        // return idx - 2 < 0 ? numbers[idx - 2 + numbers.length] : numbers[idx - 2];
+        return numbers[(2 * (k-1)) % numbers.length];
     }
 }
