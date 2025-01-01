31b5a1f9187a513e8a4cc2f589fcc4d2c1232be3
JungTae Kwon <oxdjww@gmail.com>
2025-01-01 23:33:19 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 2.83 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int[] solution(String my_string) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i < my_string.length() ; i++)
+        {
+            try{
+                list.add(Integer.parseInt(String.valueOf(my_string.charAt(i))));
+            }
+            catch(NumberFormatException e)
+            {
+                continue;
+            }
+        }
+        Collections.sort(list);
+        return list.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+    }
+}
