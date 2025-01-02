edf963fd29b22dba7f9421f8fff6c5daaac1e032
JungTae Kwon <oxdjww@gmail.com>
2025-01-02 20:31:38 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int solution(int[][] board) {
+        for(int i = 0 ; i < board.length ; i++)
+        {
+            for(int j = 0 ; j < board[i].length ; j++)
+            {
+                if(board[i][j] == 1)
+                {
+                    if(i != 0 && j != 0 && board[i-1][j-1] != 1)
+                        board[i-1][j-1] = 2;
+                    if(i != 0 && board[i-1][j] != 1)
+                        board[i-1][j] = 2;
+                    if(i != 0 && j != board[i].length-1 && board[i-1][j+1] != 1)
+                        board[i-1][j+1] = 2;
+                    if(j != 0 && board[i][j-1] != 1)
+                        board[i][j-1] = 2;
+                    if(j != board[i].length-1 && board[i][j+1] != 1)
+                        board[i][j+1] = 2;
+                    if(i != board.length-1 && j != 0 && board[i+1][j-1] != 1)
+                        board[i+1][j-1] = 2;
+                    if(i != board.length-1 && board[i+1][j] != 1)
+                        board[i+1][j] = 2;
+                    if(i != board.length-1 && j != board[i].length-1 && board[i+1][j+1] != 1)
+                        board[i+1][j+1] = 2;
+                }                    
+            }
+        }
+        int count = 0;
+        for(int i = 0 ; i < board.length ; i++)
+        {
+            for(int j = 0 ; j < board[i].length ; j++)
+            {
+                if(board[i][j] == 0)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
