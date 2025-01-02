f9b599502c7c75fa3629fd7291d75fa87aa103c6
JungTae Kwon <oxdjww@gmail.com>
2025-01-02 15:14:39 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 92.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int order) {
+        int cnt = 0;
+        while(order > 0)
+        {
+            if(order % 10 % 3 == 0 && order % 10 != 0){
+                cnt++;
+            }
+            order /= 10;
+        }
+        return cnt;
+    }
+}
