f145c99c859f81e87352c748842f788a3274d3df
JungTae Kwon <oxdjww@gmail.com>
2025-01-03 15:00:17 +0900

    [level 0] Title: 문자열 밀기, Time: 15.43 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        for(int i = 0 ; i < A.length() ; i++)
+        {
+            if(A.equals(B))
+                return answer;
+            A = A.charAt(A.length()-1 ) + A.substring(0, A.length()-1);
+            answer++;
+        }
+        return -1;
+    }
+}
