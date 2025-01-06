91827cd10926d22ed24a71cdd48cadad493689cf
JungTae Kwon <oxdjww@gmail.com>
2025-01-06 18:56:51 +0900

    [level 2] Title: 프로세스, Time: 7.57 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -14,16 +14,12 @@ class Solution {
             int pp = os.poll();
             if(!os.isEmpty() && pp < os.stream().max(Integer::compare).orElseThrow())
             {
-                System.out.println("현재최대: " + os.stream().max(Integer::compare).orElseThrow());
-                System.out.println("실행안된애 :" + pp);
                 os.add(pp);
             }else{
-                System.out.println("너 실행됨: " + pp + ", idx : " + idx);
                 counter++;
                 os.add(-1);
                 if( idx == location)
                 {
-                    System.out.println("찾았땅: " + idx);
                     return counter;
                 }
             }
