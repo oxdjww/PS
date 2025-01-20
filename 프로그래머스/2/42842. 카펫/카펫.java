3f5e7b0ff43601d00342700c2c693eb1b2b7f1f5
JungTae Kwon <oxdjww@gmail.com>
2025-01-20 23:46:56 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int total = brown + yellow;
+        for (int height = 3; height <= total; height++)
+        {
+            if (total % height == 0)
+            {
+                int width = total / height;
+                if (width < height) continue;
+                if ((width - 2) * (height - 2) == yellow)
+                {
+                    return new int[]{width, height};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
