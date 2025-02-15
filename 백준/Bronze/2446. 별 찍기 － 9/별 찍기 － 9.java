b74f570440856c1b8c37cbd1c37f85dc1eaaa761
JungTae Kwon <oxdjww@gmail.com>
2025-02-15 14:59:49 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 124 ms, Memory: 14596 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        String star = "*";
+        String blank = " ";
+        for(int i = N ; i > 0 ; i--)
+        {
+            System.out.print(blank.repeat(N-i));
+            System.out.print(star.repeat(i*2-1));
+            System.out.println();
+        }
+        for(int i = 2 ; i <= N ; i++)
+        {
+            System.out.print(blank.repeat(N-i));
+            System.out.print(star.repeat(i*2-1));
+            System.out.println();
+        }
+    }
+}
