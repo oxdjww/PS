77ab2d78363e691caf6bb1d865e1bfb9b4c6eb57
JungTae Kwon <oxdjww@gmail.com>
2025-02-25 23:46:39 +0900

    [level 1] Title: K번째수, Time: 0.45 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,19 +1,17 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
-        List<Integer> list = new ArrayList<>();
-        Arrays.stream(array).forEach(list::add);
-        
         for(int i = 0 ; i < commands.length ; i++)
         {
-            int start = commands[i][0];
+            int start = commands[i][0] - 1;
             int end = commands[i][1];
-            int target = commands[i][2];
-            List<Integer> sub = new ArrayList<>(list.subList(start-1, end));
-            Collections.sort(sub);
-            answer[i] = sub.get(target-1);
+            int target = commands[i][2] - 1;
+            
+            int[] copy = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(copy);
+            answer[i] = copy[target];
         }
         return answer;
     }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 76.6 MB, 시간: 1.19 ms
+메모리: 89.8 MB, 시간: 0.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 07일 20:00:24
+2025년 02월 25일 23:46:34
 
 ### 문제 설명
 
