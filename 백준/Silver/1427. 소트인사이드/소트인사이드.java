f20f1203c6b8cab2913244de52f7690b01fabc7b
JungTae Kwon <oxdjww@gmail.com>
2025-02-25 23:19:17 +0900

    [Silver V] Title: 소트인사이드, Time: 172 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,15 +1,24 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        String N = sc.nextLine();
-        
-        char[] digits = N.toCharArray();
-        Arrays.sort(digits);
-        
-        StringBuilder sb = new StringBuilder(new String(digits));
-        System.out.println(sb.reverse().toString());
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Integer[] arr = new Integer[str.length()];
+
+        for(int i = 0 ; i < str.length() ; i++)
+        {
+            arr[i] = str.charAt(i) - '0';
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            sb.append(arr[i]);
+        }
+        System.out.print(sb);
     }
 }
