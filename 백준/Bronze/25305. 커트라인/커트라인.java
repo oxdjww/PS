5aed2d997e5a16892f337d581d1f4e044cf6e1d9
JungTae Kwon <oxdjww@gmail.com>
2025-04-01 21:49:04 +0900

    [Bronze II] Title: 커트라인, Time: 116 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        Integer[] arr = new Integer[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i++)
+        {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.print(arr[k-1]);
+    }
+}
