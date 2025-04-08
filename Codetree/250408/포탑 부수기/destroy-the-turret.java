acc876a7137076b51d5e4ec16112d58822578d0a
oxdjww <oxdjww@gmail.com>
2025-04-08 12:07:15 +0000

    Title: 포탑 부수기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- Codetree/250408/포탑 부수기/destroy-the-turret.java
+++ Codetree/250408/포탑 부수기/destroy-the-turret.java
@@ -2,10 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static int[][] board;
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = st.nextToken();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        board = new int[n][m];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+
+        for(int i = 0; i < k; i++) {
+
+        }
     }
 }
