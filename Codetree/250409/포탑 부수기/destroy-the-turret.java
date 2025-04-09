6508bb12d61ed44f454b7ff2de7342919cb99486
oxdjww <oxdjww@gmail.com>
2025-04-09 08:27:02 +0000

    Title: 포탑 부수기, Time: 112ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- Codetree/250409/포탑 부수기/destroy-the-turret.java
+++ Codetree/250409/포탑 부수기/destroy-the-turret.java
@@ -141,7 +141,7 @@ public class Main {
         
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(board[i][j].power != 0 && (i != attacker[0] && j != attacker[1])) {
+                if(board[i][j].power != 0 && i != attacker[0] && j != attacker[1]) {
                     maxPower = Math.max(maxPower, board[i][j].power);
                 }
             }
@@ -154,7 +154,7 @@ public class Main {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(board[i][j].power != 0) {
-                    if(board[i][j].power != 0 && (i != attacker[0] && j != attacker[1])){
+                    if(board[i][j].power == maxPower && i != attacker[0] && j != attacker[1]) {
                         biggestPower.add(board[i][j]);
                     }
                 }                                       
