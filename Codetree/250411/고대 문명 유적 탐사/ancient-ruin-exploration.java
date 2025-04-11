8ab69cbd757faa8e2ef2ddcaa03cff2559698a03
oxdjww <oxdjww@gmail.com>
2025-04-11 14:39:25 +0000

    Title: 고대 문명 유적 탐사, Time: 112ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- Codetree/250411/고대 문명 유적 탐사/ancient-ruin-exploration.java
+++ Codetree/250411/고대 문명 유적 탐사/ancient-ruin-exploration.java
@@ -3,193 +3,217 @@ import java.io.*;
 
 public class Main {
     
-    private static int K;
-    private static int M;
-    private static int[][] board = new int[5][5];
-    private static Queue<Integer> spare = new LinkedList<>();
-    private static int[][] directions = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {-1, 0}};
+    private static int K;                       // 턴 수
+    private static int M;                       // 벽면 숫자 개수 (spare 큐 크기)
+    private static int[][] board = new int[5][5]; // 5×5 보드
+    private static Queue<Integer> spare = new LinkedList<>();  // spare 큐
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+    
         K = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        for(int i = 0; i < 5; i++) {
+        
+        // 보드 입력
+        for (int i = 0; i < 5; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < 5; j++) {
+            for (int j = 0; j < 5; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
+    
+        // spare 큐 입력
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) {
             spare.offer(Integer.parseInt(st.nextToken()));
         }
-
-        // print(board);
-
-        int answer = 0;
-        for(int i = 0; i < K; i++) {
-            answer += execute();
+    
+        ArrayList<Integer> turnResults = new ArrayList<>();
+        for (int turn = 0; turn < K; turn++) {
+            int turnScore = execute();
+            // 만약 어떤 턴에서 한번도 유물을 획득하지 못하면 그 즉시 탐사를 종료함.
+            if (turnScore == 0) break;
+            turnResults.add(turnScore);
         }
-        System.out.println(answer);
+        
+        // 각 턴 점수를 공백으로 구분하여 출력
+        for (int i = 0; i < turnResults.size(); i++) {
+            System.out.print(turnResults.get(i));
+            if (i < turnResults.size() - 1)
+                System.out.print(" ");
+        }
     }
-
+    
+    // 한 턴을 실행: 최적 회전 방법으로 3×3 영역 회전 후, 연쇄적으로 유물 제거(및 채우기) 수행
     private static int execute() {
-        int relicCount = 0;
-        int[] optimumConditions = findOptimum();
-
-        int optimumRow = optimumConditions[0];
-        int optimumColumn = optimumConditions[1];
-        int optimumRotate = optimumConditions[2];
+        int totalScore = 0;
+        int[] optimum = findOptimum();
+        int centerRow = optimum[0];
+        int centerCol = optimum[1];
+        int rotateCount = optimum[2];
         
-        while(optimumRotate-- < 0) {
-            rotate90(board, optimumRow, optimumColumn);
+        // 실제 board에서 해당 3×3 영역을 rotateCount번 회전 (각 회전은 90도씩)
+        for (int i = 0; i < rotateCount; i++) {
+            rotate90(board, centerRow, centerCol);
         }
-
-        // do {
-        //     relicCount += collectRelic();
-
-        //     fillRelic();
-        // } while(relicExists());
-
-        return relicCount;
-    }
-
-    private static void rotate90(int[][] matrix, int row, int column) {
-        System.out.println(row + ", " + column + " 기준으로 90도 회전");
-        // 3x3 부분 배열을 기준으로 시계방향 90도 회전
-        int[][] temp = new int[3][3];
         
-        // 3x3 배열 복사
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                temp[i][j] = matrix[row + i - 1][column + j - 1];
+        // 연쇄 제거 및 채우기 (유물이 더 이상 생성되지 않을 때까지)
+        int score;
+        do {
+            score = removeRelics(board);
+            totalScore += score;
+            if(score > 0) {
+                fillRelics(board, spare);
+            }
+        } while(score != 0);
+        
+        return totalScore;
+    }
+    
+    // 가능한 모든 회전(3×3 부분의 중심 (1,1)~(3,3), 회전각 90°, 180°, 270°)에 대해
+    // "연쇄 제거" 후 총 유물 획득 점수를 시뮬레이션하여, 
+    // (1) 전체 획득 점수가 최대, (2) 회전 각도가 작고, (3) 중심 좌표의 열, 그리고 행이 작은 순으로 선택
+    private static int[] findOptimum() {
+        int bestRow = -1, bestCol = -1, bestRotate = -1;
+        int maxScore = -1;
+        
+        // 가능한 회전 중심: 행,열 모두 1~3 (1-based)
+        for (int i = 1; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
+                // 회전 각도: k번의 90도 (k=1,2,3)
+                for (int k = 1; k <= 3; k++) {
+                    int[][] copyBoard = createCopyOf2DArray(board);
+                    Queue<Integer> copySpare = new LinkedList<>(spare);
+                    // k번 회전 적용
+                    for (int r = 0; r < k; r++) {
+                        rotate90(copyBoard, i, j);
+                    }
+                    // 연쇄 제거를 시뮬레이션하여 총 점수를 산출
+                    int score = simulateChain(copyBoard, copySpare);
+                    
+                    // 조건: (1) score 최대, (2) k 최소, (3) 중심 좌표의 열, (4) 중심 좌표의 행이 최소
+                    if (score > maxScore ||
+                       (score == maxScore && k < bestRotate) ||
+                       (score == maxScore && k == bestRotate && (j < bestCol || (j == bestCol && i < bestRow)))) {
+                        bestRow = i;
+                        bestCol = j;
+                        bestRotate = k;
+                        maxScore = score;
+                    }
+                }
             }
         }
-
-        // 90도 시계방향 회전 후 원래 위치에 다시 저장
-        matrix[row-1][column-1] = temp[2][0];
-        matrix[row-1][column] = temp[1][0];
-        matrix[row-1][column+1] = temp[0][0];
-
-        matrix[row][column-1] = temp[2][1];
-        matrix[row][column] = temp[1][1];
-        matrix[row][column+1] = temp[0][1];
-
-        matrix[row+1][column-1] = temp[2][2];
-        matrix[row+1][column] = temp[1][2];
-        matrix[row+1][column+1] = temp[0][2];
+        return new int[]{bestRow, bestCol, bestRotate};
     }
-
+    
+    // simulateChain: 주어진 board와 spare 복사본을 가지고 연쇄적으로 유물 제거 및 채우기를 수행한 후
+    // 누적 점수를 반환 (board와 spare의 실제 상태는 변경되지 않음)
+    private static int simulateChain(int[][] boardCopy, Queue<Integer> spareCopy) {
+        int totalScore = 0;
+        while (true) {
+            int score = removeRelics(boardCopy);
+            if(score == 0) break;
+            totalScore += score;
+            fillRelics(boardCopy, spareCopy);
+        }
+        return totalScore;
+    }
+    
+    // 3×3 격자 (중심 좌표 (row, col))를 시계 방향으로 90도 회전
+    private static void rotate90(int[][] matrix, int row, int col) {
+        int[][] temp = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                temp[i][j] = matrix[row + i - 1][col + j - 1];
+            }
+        }
+        
+        matrix[row - 1][col - 1] = temp[2][0];
+        matrix[row - 1][col]     = temp[1][0];
+        matrix[row - 1][col + 1] = temp[0][0];
+        
+        matrix[row][col - 1]     = temp[2][1];
+        matrix[row][col]         = temp[1][1];
+        matrix[row][col + 1]     = temp[0][1];
+        
+        matrix[row + 1][col - 1] = temp[2][2];
+        matrix[row + 1][col]     = temp[1][2];
+        matrix[row + 1][col + 1] = temp[0][2];
+    }
+    
+    // board의 깊은 복사본 반환
     private static int[][] createCopyOf2DArray(int[][] array) {
         int[][] copy = new int[5][5];
-        for(int i = 0; i < board.length; i++) {
-            copy[i] = Arrays.copyOf(board[i], board[i].length);
+        for (int i = 0; i < 5; i++) {
+            copy[i] = Arrays.copyOf(array[i], 5);
         }
         return copy;
     }
-
-    private static int[] findOptimum() {
-        int optimumRow = -1;
-        int optimumColumn = -1;
-        int optimumRotate = -1;
-        int maxCount = -1;
-        for(int i = 1; i < 4; i++) {
-            for(int j = 1; j < 4; j++) {
-                int[][] copy = createCopyOf2DArray(board);
-
-                for(int k = 1; k <= 4; k++) {
-                    rotate90(copy, i, j);
-                    List<Point> pangRelicsList = countRelics(copy);
-                    int relicsCount = 0;
-                    for(int l = 0; l < pangRelicsList.size(); l++) {
-                        relicsCount += pangRelicsList.get(l).count;
-                    }
-                    if(maxCount < relicsCount) {
-                        optimumRow = i;
-                        optimumRow = j;
-                        maxCount = relicsCount;
-                        optimumRotate = k;
-                    }
-                    System.out.println("아래 보드 유물 개수: " + relicsCount);
-                    print(copy);
-                }
-            }
-        }
-        return new int[]{optimumRow, optimumColumn, optimumRotate};
-    }
-
-    private static boolean isValidPoint(int row, int column) {
-        return row >= 0 && row < 5 && column >= 0 && column < 5;
-    }
-
-    private static List<Point> countRelics(int[][] matrix) {
-        Queue<Point> queue = new LinkedList<>();
-        List<Point> pang = new ArrayList<>();
-        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-
-        // 모든 칸 다 넣기
-        for(int row = 0; row < matrix.length; row++) {
-            for(int column = 0; column < matrix[row].length; column++) {
-                // relicsCount += dfs(matrix, false, row, column, 1, 1, visited);
-                List<int[]> tmp = new ArrayList<>();
-                tmp.add(new int[]{row, column});
-                queue.offer(new Point(row, column, 1, tmp));
-            }
-        }
-
-        while(!queue.isEmpty()) {
-            Point current = queue.poll();
-            int currentRow = current.row;
-            int currentColumn = current.column;
-            int currentCount = current.count;
-            int currentNumber = matrix[currentRow][currentColumn];
-
-            for(int i = 0; i < directions.length; i++) {
-                int nextRow = currentRow + directions[i][0];
-                int nextColumn = currentColumn + directions[i][1];
-
-                if(isValidPoint(nextRow, nextColumn) && 
-                        current.history.get(0) && 
-                        currentNumber == matrix[nextRow][nextColumn]) {
-                    System.out.println(currentRow + ", " + currentColumn + " -> " + nextRow + ", " + nextColumn + "(" + (currentCount + 1)  + ")");
-                    List<int[]> currentHistory = new ArrayList<>(current.history);
-                    currentHistory.add(new int[]{nextRow, nextColumn});
-                    queue.offer(new Point(nextRow, nextColumn, currentCount + 1, currentHistory));
-                    visited[nextRow][nextColumn] = true;
-                } else if (currentCount >= 3) {
-                    pang.add(current);
-                }
-            }
-        }
-
-        return pang;
+    
+    // board의 (row, col)가 유효한지 검사 (0~4)
+    private static boolean isValidPoint(int row, int col) {
+        return row >= 0 && row < 5 && col >= 0 && col < 5;
     }
     
-
-    private static void print(int[][] matrix) {
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 5; j++) {
-                System.out.print(matrix[i][j] + " ");
+    // 유물 제거: 상하좌우로 연결된 같은 숫자 그룹이 3개 이상이면 제거하고, 해당 그룹의 크기만큼 점수를 반환.
+    // 제거된 자리는 0으로 채움.
+    private static int removeRelics(int[][] matrix) {
+        int score = 0;
+        boolean[][] visited = new boolean[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (!visited[i][j] && matrix[i][j] != 0) {
+                    int value = matrix[i][j];
+                    Queue<int[]> queue = new LinkedList<>();
+                    List<int[]> group = new ArrayList<>();
+                    queue.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                    while (!queue.isEmpty()) {
+                        int[] cur = queue.poll();
+                        group.add(cur);
+                        int[] dr = {0, 1, 0, -1};
+                        int[] dc = {1, 0, -1, 0};
+                        for (int d = 0; d < 4; d++) {
+                            int nr = cur[0] + dr[d], nc = cur[1] + dc[d];
+                            if (isValidPoint(nr, nc) && !visited[nr][nc] && matrix[nr][nc] == value) {
+                                visited[nr][nc] = true;
+                                queue.offer(new int[]{nr, nc});
+                            }
+                        }
+                    }
+                    if (group.size() >= 3) {
+                        score += group.size();
+                        for (int[] cell : group) {
+                            matrix[cell[0]][cell[1]] = 0;
+                        }
+                    }
+                }
             }
-            System.out.println();
         }
-        System.out.println();
+        return score;
+    }
+    
+    // 중력 효과 및 빈 칸 채우기:
+    // 각 열마다 아래쪽(행 번호 큰 쪽)으로 non-zero 값들을 모으고,
+    // 남은 빈 칸에는 spare 큐에서 순서대로 숫자를 꺼내 채움.
+    // 채우는 순서는 "열 번호 오름차순, 같은 열이면 행 번호 내림차순"임.
+    private static void fillRelics(int[][] matrix, Queue<Integer> spareQueue) {
+        for (int col = 0; col < 5; col++) {
+            int pointer = 4;
+            // non-zero 값들을 아래쪽부터 채움
+            for (int row = 4; row >= 0; row--) {
+                if (matrix[row][col] != 0) {
+                    matrix[pointer][col] = matrix[row][col];
+                    pointer--;
+                }
+            }
+            // 남은 빈 칸 (pointer ~ 0)에 대해 spare 큐의 숫자로 채움
+            while (pointer >= 0) {
+                matrix[pointer][col] = spareQueue.poll();
+                pointer--;
+            }
+        }
     }
 }
-
-class Point {
-    int row, column;
-    int count;
-    List<int[]> history;
-
-    public Point(int row, int column, int count, List<int[]> history) {
-        this.row = row;
-        this.column = column;
-        this.count = count;
-        this.history = history;
-    }
-}
+    
