bf3fe89c3975a7edc93fcaea78d848f26d2ac0fa
JungTae Kwon <oxdjww@gmail.com>
2025-04-24 00:52:32 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,28 +1,16 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        
-        boolean[] dp = new boolean[N + 1];
-        
-        dp[0] = false;
-        
-        for (int i = 1; i <= N; i++) {
-            if ((i - 1 >= 0 && !dp[i - 1]) || (i - 3 >= 0 && !dp[i - 3])) {
-                dp[i] = true;
-            } else {
-                dp[i] = false;
-            }
-        }
-        
-        if (dp[N]) {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n % 2 == 1) {
             System.out.println("SK");
         } else {
             System.out.println("CY");
         }
         
-        sc.close();
     }
 }
