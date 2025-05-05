80d9041e93ed15624a37708f2ce7aa88db57eccf
JungTae Kwon <oxdjww@gmail.com>
2025-05-05 20:58:50 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 31.14 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,30 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int current = 0;
+        
         Queue<Integer> bridge = new LinkedList<>();
-        int time = 0;
-        int currentWeight = 0;
-        int idx = 0;
-
-        for (int i = 0; i < bridge_length; i++) {
-            bridge.add(0);
+        for(int i = 0; i < bridge_length; i++) {
+            bridge.offer(0);
         }
-
-        while (idx < truck_weights.length || currentWeight > 0) {
+        int index = 0;
+        int count = 0;
+        int time = 0;
+        while(count < truck_weights.length) {
             time++;
-
-            // 다리에서 트럭 제거
-            currentWeight -= bridge.poll();
-
-            // 새로운 트럭을 다리에 올릴 수 있는지 확인
-            if (idx < truck_weights.length && currentWeight + truck_weights[idx] <= weight) {
-                bridge.add(truck_weights[idx]);
-                currentWeight += truck_weights[idx];
-                idx++;
-            } else {
-                // 무게를 초과하면 빈칸 추가
-                bridge.add(0);
-            }
+            
+            // 다리에서 하차
+            int out = bridge.poll();
+            // 다리 무게 감량
+            current -= out;
+            // 진짜 트럭이면 (0아님)
+            if (out > 0) count++;
+            
+            if(index < truck_weights.length) {
+                // 새로운 트럭을 다리에 올릴 수 있는지
+                if (bridge.size() + 1 <= bridge_length && current + truck_weights[index] <= weight) {
+                    current += truck_weights[index];
+                    bridge.offer(truck_weights[index]);
+                    index++;
+                } else {
+                    // 가짜 트럭
+                    bridge.offer(0);
+                }
+            } 
         }
         return time;
     }
