3ef979217310fdcccab5627528e0838b02b1ad93
JungTae Kwon <oxdjww@gmail.com>
2025-05-06 19:07:24 +0900

    [Silver I] Title: 접두사, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/1141. 접두사/접두사.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(br.readLine());
+        }
+
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return Integer.compare(s1.length(), s2.length());
+            }
+        });
+
+        int count = n;
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j).startsWith(list.get(i))) {
+                    count--;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
