965e6e0c35862dd058b3d7aa3859846ccb2dea4d
JungTae Kwon <oxdjww@gmail.com>
2025-05-09 00:21:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 18996 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,59 +1,45 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    private static int N, K;
-    private static int nx1, nx2, nx3;
-    private static boolean[] visited = new boolean[100001];
-    private static Queue<Integer> queue = new LinkedList<>();
-
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.valueOf(st.nextToken());
-        K = Integer.valueOf(st.nextToken());
-        
-        if(N == K)
-        {
-            System.out.println(0);
-            return;
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.offer(new int[] { N, 0 });
+        boolean[] visited = new boolean[100_001];
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            int location = current[0];
+            int time = current[1];
+
+            if (location == K) {
+                System.out.println(time);
+                return;
+            }
+            int nt = time + 1;
+            if (isValid(location + 1) && !visited[location + 1]) {
+                queue.offer(new int[] { location + 1, nt });
+                visited[location + 1] = true;
+            }
+            if (isValid(location - 1) && !visited[location - 1]) {
+                queue.offer(new int[] { location - 1, nt });
+                visited[location - 1] = true;
+            }
+            if (isValid(location * 2) && !visited[location * 2]) {
+                queue.offer(new int[] { location * 2, nt });
+                visited[location * 2] = true;
+            }
         }
-    
-        nx1 = N-1;
-        nx2 = N+1;
-        nx3 = N*2;
-        if(nx1 >= 0 && nx1 <= 100000) queue.offer(nx1);
-        if(nx2 >= 0 && nx2 <= 100000) queue.offer(nx2);
-        if(nx3 >= 0 && nx3 <= 100000) queue.offer(nx3);
+    }
 
-        int time = 0;
-        int target = K;
-
-        while(true)
-        {
-            time++;
-
-            int size = queue.size();
-            for(int i = 0 ; i < size ; i++)
-            {
-                int cur = queue.poll();
-                visited[cur] = true;
-                if(cur == target)
-                {
-                    System.out.println(time);
-                    return;
-                }
-
-                nx1 = cur-1;
-                nx2 = cur+1;
-                nx3 = cur*2;
-                    
-                if(nx1 >= 0 && nx1 <= 100000 && !visited[nx1]) queue.offer(nx1);
-                if(nx2 >= 0 && nx2 <= 100000 && !visited[nx2]) queue.offer(nx2);
-                if(nx3 >= 0 && nx3 <= 100000 && !visited[nx3]) queue.offer(nx3);
-            }            
-        }
+    private static boolean isValid(int location) {
+        return location >= 0 && location <= 100_000;
     }
 }
