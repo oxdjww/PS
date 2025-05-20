09664d329332cf8ac13c2147368ae38963cba4ad
JungTae Kwon <oxdjww@gmail.com>
2025-05-20 19:55:39 +0900

    [Silver III] Title: 주유소, Time: 308 ms, Memory: 30356 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] cost = new int[n];
+        int[] dist = new int[n - 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n - 1; i++) {
+            dist[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+        }
+        int optimalPrice = Integer.MAX_VALUE;
+        int current = 0;
+        for (int i = 0; i < n - 1; i++) {
+            current += Math.min(optimalPrice, cost[i]) * dist[i];
+            optimalPrice = Math.min(optimalPrice, cost[i]);
+        }
+
+        System.out.println(current);
+
+    }
+}
