3790221214919dce8f0942acb7fd5ae37acc9c70
JungTae Kwon <oxdjww@gmail.com>
2025-06-11 22:44:59 +0900

    [Silver II] Title: N과 M (9), Time: 336 ms, Memory: 54920 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    private static int N;
+    private static int M;
+    private static int[] arr;
+    private static ArrayList<List<Integer>> answer = new ArrayList<>();
+    private static HashSet<String> hs = new HashSet<>();
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(new boolean[N], new ArrayList<>());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < answer.size(); i++) {
+            List<Integer> list = answer.get(i);
+            for (int idx : list) {
+                sb.append(idx).append(" ");
+            }
+            if (i != answer.size() - 1)
+                sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void dfs(boolean[] visited, List<Integer> list) {
+        if (list.size() == M) {
+            String made = generateString(list);
+            if (hs.contains(made)) {
+                return;
+            }
+            answer.add(new ArrayList<>(list));
+            hs.add(made);
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                list.add(arr[i]);
+                visited[i] = true;
+                dfs(visited, list);
+                list.remove(list.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    private static String generateString(List<Integer> list) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+            if (i != list.size() - 1)
+                sb.append(" ");
+        }
+        return sb.toString();
+    }
+}
