63c27e55b73a3d6fdfb3dac56e4c018d08a5f9ba
JungTae Kwon <oxdjww@gmail.com>
2025-06-11 22:31:55 +0900

    [Silver III] Title: N과 M (8), Time: 184 ms, Memory: 18872 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    private static int N;
+    private static int M;
+    private static int[] arr;
+    private static ArrayList<List<Integer>> answer = new ArrayList<>();
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0, new ArrayList<>());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < answer.size(); i++) {
+            List<Integer> list = answer.get(i);
+            for (int idx : list) {
+                sb.append(idx).append(" ");
+            }
+            if (i != answer.size() - 1)
+                sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void dfs(int current, List<Integer> list) {
+        if (list.size() == M) {
+            answer.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = current; i < N; i++) {
+            list.add(arr[i]);
+            dfs(i, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
